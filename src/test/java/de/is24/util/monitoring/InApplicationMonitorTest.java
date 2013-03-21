a6d16d7eab67513a998686d963288c384b14c4b0
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-03-21 22:47:32 +0100

    test to verify that explicit initialization will keep allready registered values

---
+++ src/test/java/de/is24/util/monitoring/InApplicationMonitorTest.java
@@ -0,0 +1,56 @@
+package de.is24.util.monitoring;
+
+import de.is24.util.monitoring.jmx.JmxAppMon4JNamingStrategy;
+import de.is24.util.monitoring.keyhandler.DefaultKeyEscaper;
+import de.is24.util.monitoring.tools.DoNothingReportVisitor;
+import org.junit.Before;
+import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
+
+
+public class InApplicationMonitorTest {
+  private static final String INITIALIZED_COUNTER_1 = "initialized.counter.1";
+
+  @Before
+  public void setupClass() {
+    InApplicationMonitor.resetInstanceForTesting();
+  }
+
+  @Test
+  public void explicitInitializationShouldTakeOverInitializedCountersAndTimers() {
+    InApplicationMonitor.getInstance().initializeCounter(INITIALIZED_COUNTER_1);
+
+
+    DefaultKeyEscaper keyEscaper = new DefaultKeyEscaper();
+    CorePlugin corePlugin = new CorePlugin(new JmxAppMon4JNamingStrategy() {
+        @Override
+        public String getJmxPrefix() {
+          return "lala";
+        }
+      }, keyEscaper);
+    InApplicationMonitor explicitInitializedInApplicationMonitor = InApplicationMonitor.initInstance(
+      corePlugin, keyEscaper);
+
+    CheckForCounterExistenceReportVisitor reportVisitor = new CheckForCounterExistenceReportVisitor(
+      INITIALIZED_COUNTER_1);
+    explicitInitializedInApplicationMonitor.getCorePlugin().reportInto(reportVisitor);
+    assertThat(reportVisitor.found).isTrue();
+  }
+
+  private static class CheckForCounterExistenceReportVisitor extends DoNothingReportVisitor {
+    private final String counterName;
+    private boolean found = false;
+
+    CheckForCounterExistenceReportVisitor(String counterName) {
+      this.counterName = counterName;
+    }
+
+    @Override
+    public void reportCounter(Counter counter) {
+      if (counter.getName().equals(counterName)) {
+        found = true;
+      }
+    }
+
+  }
+}
---
+++ src/test/java/de/is24/util/monitoring/tools/DoNothingReportVisitor.java
@@ -0,0 +1,34 @@
+package de.is24.util.monitoring.tools;
+
+import de.is24.util.monitoring.Counter;
+import de.is24.util.monitoring.HistorizableList;
+import de.is24.util.monitoring.ReportVisitor;
+import de.is24.util.monitoring.StateValueProvider;
+import de.is24.util.monitoring.Timer;
+import de.is24.util.monitoring.Version;
+
+
+/**
+ * convenience class for easy selective override of ReportVisitor
+ */
+public class DoNothingReportVisitor implements ReportVisitor {
+  @Override
+  public void reportCounter(Counter counter) {
+  }
+
+  @Override
+  public void reportTimer(Timer timer) {
+  }
+
+  @Override
+  public void reportStateValue(StateValueProvider stateValueProvider) {
+  }
+
+  @Override
+  public void reportHistorizableList(HistorizableList historizableList) {
+  }
+
+  @Override
+  public void reportVersion(Version version) {
+  }
+}
