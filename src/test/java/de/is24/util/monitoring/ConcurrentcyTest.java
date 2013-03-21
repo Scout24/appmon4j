383b0cad866a928a0cd708dfba69007f770cc1da
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-03-21 14:21:00 +0100

    add external initialization to concurrency test to validate problem of missing JMX Bean after multi thread initialization of InApplicationMonitor

--- src/test/java/de/is24/util/monitoring/ConcurrentcyTest.java
+++ src/test/java/de/is24/util/monitoring/ConcurrentcyTest.java
@@ -1,5 +1,9 @@
 package de.is24.util.monitoring;
 
+import de.is24.util.monitoring.jmx.SimpleJmxAppmon4jNamingStrategy;
+import de.is24.util.monitoring.keyhandler.DefaultKeyEscaper;
+import de.is24.util.monitoring.keyhandler.KeyHandler;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import java.text.NumberFormat;
@@ -8,12 +12,15 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
 
 
 public class ConcurrentcyTest {
   private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance();
+  private static final Logger LOGGER = Logger.getLogger(ConcurrentcyTest.class);
 
   static {
     NUMBER_FORMAT.setGroupingUsed(true);
@@ -24,28 +31,42 @@ public class ConcurrentcyTest {
   private static final int THREADS = 10, RUNS = 10, LOOPS = 10;
 
   private ThreadPoolExecutor executor;
-  private Job observerJob, timerJob;
+  private Job observerJob, timerJob, initJob;
   private Random rnd;
 
   private AtomicLong exceptions;
+  private AtomicLong successfullInits;
+  private AtomicLong failedInits;
 
   @Before
   public void setUp() throws Exception {
     this.observerJob = new ObserverJob();
     this.timerJob = new TimerJob();
+    this.initJob = new InitJob();
     this.rnd = new Random();
     this.exceptions = new AtomicLong();
+    this.successfullInits = new AtomicLong();
+    this.failedInits = new AtomicLong();
   }
 
   @Test
   public void testConcurrency() throws InterruptedException {
     for (int i = 0; i < LOOPS; i++) {
+      LOGGER.info("#########################   run " + i + " ###########################");
+      LOGGER.info("#########################   run " + i + " ###########################");
+      LOGGER.info("#########################   run " + i + " ###########################");
+      LOGGER.info("#########################   run " + i + " ###########################");
       InApplicationMonitor.resetInstanceForTesting();
+      assertFalse(JMXTestHelper.checkInApplicationMonitorJMXBeanRegistered());
+
       createExecutor(THREADS);
 
-      executeJobs(timerJob, observerJob);
+      executeJobs(timerJob, observerJob, initJob);
+      assertTrue(JMXTestHelper.checkInApplicationMonitorJMXBeanRegistered());
     }
     assertThat(exceptions.get(), is(0L));
+    assertThat(successfullInits.get(), is((long) LOOPS));
+
 
   }
 
@@ -71,6 +92,7 @@ public class ConcurrentcyTest {
       try {
         doJob();
       } catch (Exception e) {
+        LOGGER.info("oops", e);
         exceptions.incrementAndGet();
       }
     }
@@ -104,4 +126,20 @@ public class ConcurrentcyTest {
     }
 
   }
+
+  private class InitJob extends Job {
+    @Override
+    protected void doJob() {
+      KeyHandler keyEscaper = new DefaultKeyEscaper();
+      try {
+        CorePlugin corePlugin = new CorePlugin(new SimpleJmxAppmon4jNamingStrategy("is24"), keyEscaper);
+        InApplicationMonitor.initInstance(corePlugin, keyEscaper);
+        successfullInits.incrementAndGet();
+      } catch (IllegalStateException e) {
+        failedInits.incrementAndGet();
+      }
+    }
+
+  }
+
 }
