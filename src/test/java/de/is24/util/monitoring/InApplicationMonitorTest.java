cd77253785e5d8673a31d357d96025c65a0146ca
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-03-28 19:08:13 +0100

    make tests more specific, to explain contract better

--- src/test/java/de/is24/util/monitoring/InApplicationMonitorTest.java
+++ src/test/java/de/is24/util/monitoring/InApplicationMonitorTest.java
@@ -16,10 +16,25 @@ public class InApplicationMonitorTest {
     InApplicationMonitor.resetInstanceForTesting();
   }
 
+
   @Test
-  public void explicitInitializationShouldTakeOverInitializedCountersAndTimers() {
+  public void explicitInitializationShouldKeepInApplicationMonitorInstance() {
+    // given a default configured InApplication Monitor
+    InApplicationMonitor defaultInstance = InApplicationMonitor.getInstance();
+
+    // when explicitly initializing InApplicationMonitor
+    initializeWithJMXNaming();
+
+    // then the new CorePlugin Instance is used
+    assertThat(InApplicationMonitor.getInstance()).isSameAs(defaultInstance);
+  }
+
+  @Test
+  public void explicitInitializationShouldSetCorePlugin() {
+    // given a default configured InApplication Monitor with a counter added
     InApplicationMonitor.getInstance().initializeCounter(INITIALIZED_COUNTER_1);
 
+    // when explicitly initializing InApplicationMonitor
     DefaultKeyEscaper keyEscaper = new DefaultKeyEscaper();
     CorePlugin corePlugin = new CorePlugin(new JmxAppMon4JNamingStrategy() {
         @Override
@@ -30,15 +45,66 @@ public class InApplicationMonitorTest {
     InApplicationMonitor explicitInitializedInApplicationMonitor = InApplicationMonitor.initInstance(
       corePlugin, keyEscaper);
 
+    // then the new CorePlugin Instance is used
+    assertThat(explicitInitializedInApplicationMonitor.getCorePlugin()).isSameAs(corePlugin);
+  }
+
+
+  @Test
+  public void explicitInitializationShouldTakeOverInitializedCountersAndTimersIntoCorePlugin() {
+    // given a default configured InApplication Monitor with a counter added
+    InApplicationMonitor.getInstance().initializeCounter(INITIALIZED_COUNTER_1);
+
+    // when explicitly initializing InApplicationMonitor
+    CorePlugin corePlugin = initializeWithJMXNaming();
+
+    // then
     CheckForCounterExistenceReportVisitor reportVisitor = new CheckForCounterExistenceReportVisitor(
       INITIALIZED_COUNTER_1);
-    assertThat(explicitInitializedInApplicationMonitor.getCorePlugin()).isSameAs(corePlugin);
-    explicitInitializedInApplicationMonitor.getCorePlugin().reportInto(reportVisitor);
+    corePlugin.reportInto(reportVisitor);
     assertThat(reportVisitor.found).isTrue();
-    assertThat(JMXTestHelper.checkInApplicationMonitorJMXBeanRegistered("lala")).isEqualTo(true);
 
+  }
+
+  @Test
+  public void explicitInitializationShouldTakeOverInitializedCountersAndTimersIntoJMX() {
+    // given a default configured InApplication Monitor with a counter added
+    InApplicationMonitor.getInstance().initializeCounter(INITIALIZED_COUNTER_1);
+
+    // when explicitly initializing InApplicationMonitor
+    initializeWithJMXNaming();
+
+    // then
     assertThat(JMXTestHelper.getCounterValue("lala", INITIALIZED_COUNTER_1)).isEqualTo(0);
   }
+
+
+  @Test
+  public void explicitInitializationWithJMXNamingStrategyShouldInitializeJMX() {
+    // given a default configured InApplication Monitor
+    InApplicationMonitor.getInstance();
+
+    // when explicitly initializing InApplicationMonitor
+    initializeWithJMXNaming();
+
+    // then
+    assertThat(JMXTestHelper.checkInApplicationMonitorJMXBeanRegistered("lala")).isEqualTo(true);
+  }
+
+
+  private CorePlugin initializeWithJMXNaming() {
+    DefaultKeyEscaper keyEscaper = new DefaultKeyEscaper();
+    CorePlugin corePlugin = new CorePlugin(new JmxAppMon4JNamingStrategy() {
+        @Override
+        public String getJmxPrefix() {
+          return "lala";
+        }
+      }, keyEscaper);
+    InApplicationMonitor explicitInitializedInApplicationMonitor = InApplicationMonitor.initInstance(
+      corePlugin, keyEscaper);
+    return corePlugin;
+  }
+
 
   private static class CheckForCounterExistenceReportVisitor extends DoNothingReportVisitor {
     private final String counterName;
