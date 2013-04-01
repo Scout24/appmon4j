05ad70370b240c6cbac47bf8e8fdc506d70c884a
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-04-01 09:32:03 +0200

    remove useless default comment

--- src/main/java/de/is24/util/monitoring/measurement/MeasurementHandler.java
+++ src/main/java/de/is24/util/monitoring/measurement/MeasurementHandler.java
@@ -1,12 +1,5 @@
 package de.is24.util.monitoring.measurement;
 
-/**
- * Created with IntelliJ IDEA.
- * User: oschmitz
- * Date: 20.09.12
- * Time: 12:34
- * To change this template use File | Settings | File Templates.
- */
 public interface MeasurementHandler {
   void handle(String monitorName, long measurement);
 }
