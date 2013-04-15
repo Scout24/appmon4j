4a9a39998b5e4600308ae2847064bc15d273ae4a
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-04-15 23:16:51 +0200

    Send MultiValues to Graphite

--- src/main/java/de/is24/util/monitoring/state2graphite/StateValuesToGraphite.java
+++ src/main/java/de/is24/util/monitoring/state2graphite/StateValuesToGraphite.java
@@ -1,11 +1,14 @@
 package de.is24.util.monitoring.state2graphite;
 
 import de.is24.util.monitoring.InApplicationMonitor;
+import de.is24.util.monitoring.MultiValueProvider;
 import de.is24.util.monitoring.Reportable;
 import de.is24.util.monitoring.ReportableObserver;
+import de.is24.util.monitoring.State;
 import de.is24.util.monitoring.StateValueProvider;
 import de.is24.util.monitoring.tools.LocalHostNameResolver;
 import org.apache.log4j.Logger;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -17,6 +20,7 @@ public class StateValuesToGraphite implements ReportableObserver {
   private static final Logger LOGGER = Logger.getLogger(StateValuesToGraphite.class);
   private ScheduledExecutorService ex;
   private Map<String, StateValueProvider> stateValues;
+  private Map<String, MultiValueProvider> multiValueProviders;
 
   public StateValuesToGraphite(String graphiteHost, int graphitePort, String appName) {
     this(appName, new LocalHostNameResolver(), new GraphiteConnection(graphiteHost, graphitePort));
@@ -27,6 +31,7 @@ public class StateValuesToGraphite implements ReportableObserver {
                         GraphiteConnection graphiteClient) {
     String keyPrefix = appName + "." + localHostNameResolver.getLocalHostName() + ".states";
     stateValues = new ConcurrentHashMap<String, StateValueProvider>();
+    multiValueProviders = new ConcurrentHashMap<String, MultiValueProvider>();
     InApplicationMonitor.getInstance().getCorePlugin().addReportableObserver(this);
     ex = Executors.newSingleThreadScheduledExecutor();
     ex.scheduleAtFixedRate(new ReportStateValuesJob(graphiteClient, keyPrefix), 1, 10, TimeUnit.SECONDS);
@@ -37,6 +42,8 @@ public class StateValuesToGraphite implements ReportableObserver {
   public void addNewReportable(Reportable reportable) {
     if (reportable instanceof StateValueProvider) {
       stateValues.put(reportable.getName(), (StateValueProvider) reportable);
+    } else if (reportable instanceof MultiValueProvider) {
+      multiValueProviders.put(reportable.getName(), (MultiValueProvider) reportable);
     }
   }
 
@@ -62,6 +69,12 @@ public class StateValuesToGraphite implements ReportableObserver {
       Long curTimeInSec = System.currentTimeMillis() / 1000;
       StringBuilder lines = new StringBuilder();
 
+      addStateValues(curTimeInSec, lines);
+      addMultiStates(curTimeInSec, lines);
+      graphiteClient.send(lines.toString());
+    }
+
+    private void addStateValues(Long curTimeInSec, StringBuilder lines) {
       for (StateValueProvider stateValueProvider : stateValues.values()) {
         try {
           StringBuilder line = new StringBuilder();
@@ -80,7 +93,25 @@ public class StateValuesToGraphite implements ReportableObserver {
           LOGGER.warn("getting StateValue failed for " + stateValueProvider.getName());
         }
       }
-      graphiteClient.send(lines.toString());
+    }
+
+    private void addMultiStates(Long curTimeInSec, StringBuilder lines) {
+      for (MultiValueProvider multiValueProvider : multiValueProviders.values()) {
+        Collection<State> values = multiValueProvider.getValues();
+        for (State state : values) {
+          StringBuilder line = new StringBuilder();
+          line.append(keyPrefix)
+          .append(".")
+          .append(state.name)
+          .append(" ")
+          .append(state.value)
+          .append(" ")
+          .append(curTimeInSec)
+          .append("\n");
+
+          lines.append(line.toString());
+        }
+      }
     }
 
   }
