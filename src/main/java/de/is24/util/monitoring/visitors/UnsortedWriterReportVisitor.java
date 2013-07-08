34feb24d238095933d1444fb2cc866677dae02fb
Lars Gohlke <lars.gohlke@immobilienscout24.de>
2013-07-08 11:04:41 +0200

    fix Multithreaded correctness - Call to static DateFormat As the JavaDoc states, DateFormats are inherently unsafe for multithreaded use. The detector has found a call to an instance of DateFormat that has been obtained via a static field. This looks suspicous. For more information on this see Sun Bug #6231579 and Sun Bug #6178997.

--- src/main/java/de/is24/util/monitoring/visitors/UnsortedWriterReportVisitor.java
+++ src/main/java/de/is24/util/monitoring/visitors/UnsortedWriterReportVisitor.java
@@ -1,19 +1,21 @@
 package de.is24.util.monitoring.visitors;
 
-import java.io.IOException;
-import java.io.Writer;
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import de.is24.util.monitoring.MultiValueProvider;
-import de.is24.util.monitoring.State;
-import org.apache.log4j.Logger;
 import de.is24.util.monitoring.Counter;
 import de.is24.util.monitoring.Historizable;
 import de.is24.util.monitoring.HistorizableList;
+import de.is24.util.monitoring.MultiValueProvider;
 import de.is24.util.monitoring.ReportVisitor;
+import de.is24.util.monitoring.State;
 import de.is24.util.monitoring.StateValueProvider;
 import de.is24.util.monitoring.Timer;
 import de.is24.util.monitoring.Version;
+import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Collection;
 
 
 /**
@@ -21,7 +23,7 @@ import de.is24.util.monitoring.Version;
  */
 public abstract class UnsortedWriterReportVisitor implements ReportVisitor {
   private static final Logger LOGGER = Logger.getLogger(UnsortedWriterReportVisitor.class);
-  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+  private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss");
 
   protected Writer writer;
 
@@ -98,9 +100,16 @@ public abstract class UnsortedWriterReportVisitor implements ReportVisitor {
     StringBuffer result = new StringBuffer();
     result.append(aHistorizableList.getName()).append(" historizable :\n");
     for (Historizable historizable : aHistorizableList) {
-      result.append(DATE_FORMAT.format(historizable.getTimestamp())).append(" ");
+      String timestamp = getFormattedDate(historizable);
+      result.append(timestamp).append(" ");
       result.append(historizable.getValue()).append("\n");
     }
     writeStringToWriter(result.toString());
+  }
+
+  private String getFormattedDate(final Historizable historizable) {
+    long milliSecondsSince1970 = historizable.getTimestamp().getTime();
+    DateTime dateTime = new DateTime().withMillis(milliSecondsSince1970);
+    return dateTime.toString(DATE_FORMAT);
   }
 }
